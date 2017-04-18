258301a03dabbc70b057e19d9aeaa02050727fdd
esteban1234 <tva_libra@hotmail.com>
2017-04-18 16:40:36 -0500

    inicio

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
 <article class="contenheader">
 <!-- <p></p> -->
 	<header>
-		<i class="fa fa-bars menu-abrir" aria-hidden="true"></i>
-		<i class="fa fa-sort-asc menu-cerrar" aria-hidden="true"></i>
-		<figure><img src="img/chipahua.png" class="img-responsive" alt=""></figure>
+	
+		<figure><img src="img/impeza.png" class="img-responsive" alt=""></figure>
 		<div id="menu" class="menu">
 			<ul>
 				<li class="select"><a href=""><span><i class="fa fa-home" aria-hidden="true"></i></span>&nbsp;INICIO</a></li>
@@ -17,14 +16,16 @@
 		</div>
 	</header>
 
+	
+
 	<article class="banner">
-		<p class="tit text-info">LIMPIEZA COMPLETA!</p>
+		<p class="tit text-primary">LIMPIEZA COMPLETA!</p>
 		<p class="subtit">Limpiamos los hogares y centros de trabajo</p>
 	</article>
 
 	<div class="btn-toolbar botonb" role="toolbar">
 		  <a href="vistas/contacto.php">
-		  	<button onclick="" type="button" class="btn btn-info btn-lg">
+		  	<button onclick="" type="button" class="btn btn-primary btn-lg">
 		    	<span><i class="fa fa-plus" aria-hidden="true"></i></span> Contáctanos
 		  	</button>
 		  </a>
@@ -33,7 +34,7 @@
 
 
 <article class="contenedor">
-<h1 class="alserv text-info">Algunos de nuestros servicios</h1>
+<h1 class="alserv text-primary">Algunos de nuestros servicios</h1>
 
 <article class="contenserv">
 <div class="contendivs">
@@ -46,10 +47,10 @@
 	</div>
 
 	<div class="imagen2">
-	<article class="icono2"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Productos de limpieza</article>
+	<article class="icono2"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;CCTV</article>
 		<div class="info">
-			<p class="headline">Productos de Limpieza</p>
-			<p class="info2">Ofrecemos una gran variedad de productos de limpieza para el hogar o industriales.</p>
+			<p class="headline">CCTV</p>
+			<p class="info2">Digitales y analógicas. Cámaras IP, para vigilancia nocturna, fijas y PTZ.</p>
 		</div>
 	</div>
 
@@ -65,7 +66,7 @@
 <!-- <p class="btn btn-info"><a href="">SERVICIOS</a></p> -->
 <div class="btn-toolbar boton" role="toolbar">
   <a href="vistas/servicios.php">
-		  	<button onclick="" type="button" class="btn btn-info btn-lg">
+		  	<button onclick="" type="button" class="btn btn-primary btn-lg">
 		    	<span><i class="fa fa-plus" aria-hidden="true"></i></span> Servicios
 		  	</button>
 		  </a>
@@ -77,7 +78,7 @@
 	<p>Somos la mejor inversión en la vigilancia de sus oficinas.</p>
 	<div class="btn-toolbar boton" role="toolbar">
   <a href="vistas/contacto.php">
-		  	<button onclick="" type="button" class="btn btn-info btn-lg">
+		  	<button onclick="" type="button" class="btn btn-primary btn-lg">
 		    	<span><i class="fa fa-plus" aria-hidden="true"></i></span> Contáctanos
 		  	</button>
 		  </a>
@@ -85,12 +86,13 @@
 </article>
 
 </article>
+
 <footer>
-	<p class="textpie">Derechos de autor 2017© | LIMPIEZA Y VIGILANCIA ESPECIALIZADA CHIPAUA S.A. de C.V.</p>
-	<p class="textpie2">info@chipahua.com</p>
+	<p class="textpie">Derechos de autor 2017© | IMPEZA S.A. de C.V.</p>
+	<p class="textpie2">info@impeza.com</p>
 </footer>
 
-<span class="ir-arriba icon-chevron-up"></span>
+<!-- <span class="ir-arriba icon-chevron-up"></span> -->
 <script src="js/jquery.js"></script>
 <script src="js/menu.js"></script>
 <script type="text/javascript" src="js/iconoarriba.js"></script>
