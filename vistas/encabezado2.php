7f2598dd1ac6fe63615c16373433fd9cf3616e13
esteban1234 <tva_libra@hotmail.com>
2017-04-25 13:51:05 -0500

    codigo de verificacion

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="O45gaTHXWjUOq_Giv66ZCAKiIrnwcw6Jc78VdY99QqQ" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>IMPEZA</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
